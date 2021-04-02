a743add6a8119de1d81e494bd6938b77c0c1b03a
Juan Santiago <fact4bass@Juans-MBP.fios-router.home>
2021-04-02 15:16:30 -0400

    added results

---
+++ OperationExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
+
+
+
+        }
+    }
+}
